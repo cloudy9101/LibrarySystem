731f01f7392b482e63dcdc4a1642ef564b3e1139
cloudy9101 <cloudy9101@gmail.com>
2019-11-06 12:50:13 +1300

    Add implementation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :book
+  BOOK_STOCK_IS_EMPTY = "Selected book's stock is empty.".freeze
+  BOOK_IS_NOT_EXIST = "The book isn't exist.".freeze
+
+  def self.make(username, book_id)
+    book = Book.find_by(id: book_id)
+    return BOOK_IS_NOT_EXIST unless book
+    return BOOK_STOCK_IS_EMPTY unless book.count.positive?
+
+    booking = new(book_id: book.id, username: username)
+    booking.save
+    book.update(count: book.count - 1)
+
+    booking
+  end
 end
